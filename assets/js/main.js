be138af1d56b76c46684da2313de720086c90555
Giuditta <giudittamarino89@gmail.com>
2021-03-16 15:59:20 +0100

    ESERCIZIO PERFEZIONATO CON INPUT E BUTTON

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,73 +1,76 @@
-// CREO UN OGGETTO CHE DESCRIVA UNO STUDENTE CON LE PROPRIETA' NOME, COGNOME ED ETA'
-var studente = {
-  nome: 'Giuditta',
-  cognome: 'Marino',
-  età: '31'
-}
+$(document).ready(function() {
 
-// STAMPO A SCHERMO TUTTE LE PROPRIETA' DELL'OGGETTO STUDENTE
-console.log(studente);
-for (var key in studente) {
-  console.log(key, studente[key]);
-}
-
-// CREO UN ARRAY DI OGGETTI DI STUDENTI
-var studenti = [
-  {
+  // CREO UN OGGETTO CHE DESCRIVA UNO STUDENTE CON LE PROPRIETA' NOME, COGNOME ED ETA'
+  var studente = {
     nome: 'Giuditta',
     cognome: 'Marino',
     età: '31'
-  },
-  {
-    nome: 'Barbara',
-    cognome: 'Rossi',
-    età: '40'
-  },
-
-  {
-    nome: 'Antonia',
-    cognome: 'Bianchi',
-    età: '29'
-  },
-
-  {
-    nome: 'Alessia',
-    cognome: 'Neri',
-    età: '34'
-  },
-
-  {
-    nome: 'Jessica',
-    cognome: 'Verdi',
-    età: '26'
   }
-]
 
-console.log(studenti);
-
-// STAMPO PER OGNI STUDENTE NOME E COGNOME
-for (var i = 0; i < studenti.length; i++) {
-  console.log(studenti[i]['nome'], studenti[i]['cognome']);
-}
-
-aggiungiStudente();
-
-// CREO TRE PROMPT IN CUI FACCIO INSERIRE ALL'UTENTE NOME COGNOME ED ETA E INSERISCO L'OGGETTO NELL'ARRAY DI STUDENTI
-function aggiungiStudente(){
-  // CREO TRE PROMPT IN CUI FACCIO INSERIRE ALL'UTENTE NOME COGNOME ED ETA
-  var nome = prompt('Inserisci il nome dello studente');
-  var cognome = prompt('Inserisci il cognome dello studente');
-  var eta = prompt("inserisci l'età dello studente");
-
-  // CREO UN OGGETTO NUOVOSTUDENTE CHE CONTIENE LE PROPRIETA' INSERITE TRAMITE PROMPT
-  var nuovoStudente = {
-    nome: nome,
-    cognome: cognome,
-    età: eta
+  // STAMPO A SCHERMO TUTTE LE PROPRIETA' DELL'OGGETTO STUDENTE
+  console.log(studente);
+  for (var key in studente) {
+    console.log(key, studente[key]);
   }
-  console.log(nuovoStudente)
 
-  // PUSHO NELL'ARRAY IL NUOVO OGGETTO
-  studenti.push(nuovoStudente);
+  // CREO UN ARRAY DI OGGETTI DI STUDENTI
+  var studenti = [
+    {
+      nome: 'Giuditta',
+      cognome: 'Marino',
+      età: '31'
+    },
+    {
+      nome: 'Barbara',
+      cognome: 'Rossi',
+      età: '40'
+    },
+
+    {
+      nome: 'Antonia',
+      cognome: 'Bianchi',
+      età: '29'
+    },
+
+    {
+      nome: 'Alessia',
+      cognome: 'Neri',
+      età: '34'
+    },
+
+    {
+      nome: 'Jessica',
+      cognome: 'Verdi',
+      età: '26'
+    }
+  ]
+
   console.log(studenti);
-}
+
+  // STAMPO PER OGNI STUDENTE NOME E COGNOME
+  for (var i = 0; i < studenti.length; i++) {
+    console.log(studenti[i]['nome'], studenti[i]['cognome']);
+  }
+
+  // AL CLICK SUL BUTTON AGGIUNGI
+  $('#aggiungi').click(function() {
+    // SALVO IN DELLE VARIABILI I VALUE DEGLI INPUT TRAMITE CUI L'UTENTE INSERISCE NOME COGNOME ED ETA'
+    var nome = $('#nome').val();
+    var cognome = $('#cognome').val();
+    var eta = $('#eta').val();
+
+    // CREO UN OGGETTO NUOVOSTUDENTE CHE CONTIENE LE PROPRIETA' INSERITE TRAMITE INPUT NEL DOM
+    var nuovoStudente = {
+      nome: nome,
+      cognome: cognome,
+      età: eta
+    }
+    console.log(nuovoStudente)
+    // PUSHO NELL'ARRAY IL NUOVO OGGETTO
+    studenti.push(nuovoStudente);
+
+    console.log(studenti);
+  })
+  
+
+})
