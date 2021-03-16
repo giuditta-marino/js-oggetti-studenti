86b0602ee6d84f970f7ba9eae2809130dd0b7b7a
Giuditta <giudittamarino89@gmail.com>
2021-03-16 14:44:15 +0100

    creato primo oggetto, creato array con oggetti

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+// CREO UN OGGETTO CHE DESCRIVA UNO STUDENTE CON LE PROPRIETA' NOME, COGNOME ED ETA'
+var studente = {
+  nome: 'Giuditta',
+  cognome: 'Marino',
+  età: '31'
+}
+
+// STAMPO A SCHERMO TUTTE LE PROPRIETA'
+console.log(studente);
+
+// CREO UN ARRAY DI OGGETTI DI STUDENTI
+var studenti = [
+  {
+    nome: 'Giuditta',
+    cognome: 'Marino',
+    età: '31'
+  },
+  {
+    nome: 'Barbara',
+    cognome: 'Rossi',
+    età: '40'
+  },
+
+  {
+    nome: 'Antonia',
+    cognome: 'Bianchi',
+    età: '29'
+  },
+
+  {
+    nome: 'Alessia',
+    cognome: 'Neri',
+    età: '34'
+  },
+
+  {
+    nome: 'Giuditta',
+    cognome: 'Marino',
+    età: '31'
+  }
+]
+
+console.log(studenti);
